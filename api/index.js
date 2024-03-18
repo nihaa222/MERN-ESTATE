2ea6bfc54b853f19a08399a27c02ae4f780b38f1
Niharika Deb <127780989+nihaa222@users.noreply.github.com>
2024-03-18 13:44:02 +0530

    not connected

--- api/index.js
+++ api/index.js
@@ -2,17 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("connected to db");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-1;
-
 const app = express();
 
 app.listen(3000, () => {
