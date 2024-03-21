0e07a4b620476df96f354b521d14c7ce34f10ca8
Niharika Deb <127780989+nihaa222@users.noreply.github.com>
2024-03-21 12:52:46 +0530

    profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,43 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          className="h-24 w-24 rounded-full object-cover cursor-pointer m-2 self-center "
+        ></img>
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg "
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg "
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg "
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg uppercase hover:opaity-95 disabled:opacity-80 p-3">
+          update
+        </button>
+        <div className="flex justify-between mt-5">
+          <span className="text-red-700 cursor-pointer">Delete Account</span>
+          <span className="text-red-700 cursor-pointer">Sign out</span>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
